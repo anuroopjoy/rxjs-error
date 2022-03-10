b3547ccb14c58ce676aa33c2ef81ddff482e75b2
Anuroop Joy <anuroop.joy@hrblock.com>
2022-03-10 11:32:32 +0530

    init

---
+++ src/app/child1/child1.component.ts
@@ -0,0 +1,61 @@
+import { Component, OnInit } from '@angular/core';
+import {
+  catchError,
+  delayWhen,
+  finalize,
+  Observable,
+  of,
+  retry,
+  retryWhen,
+  tap,
+  throwError,
+  timer,
+} from 'rxjs';
+import { SharedService } from '../shared.service';
+
+@Component({
+  selector: 'app-child1',
+  templateUrl: './child1.component.html',
+  styleUrls: ['./child1.component.scss'],
+})
+export class Child1Component implements OnInit {
+  public data!: any;
+  private form!: any;
+  constructor(private sharedService: SharedService) {}
+
+  ngOnInit(): void {
+    try {
+      this.sharedService
+        .getData()
+        // .pipe(catchError(() => of('Http call failed')))
+        .pipe(
+          // catchError((err) => {
+          //   console.log('rethrowing error', err);
+          //   return throwError(() => new Error(err.message));
+          // }),
+          // finalize(() => {
+          //   console.log('finally block 1');
+          // }),
+          retry({ count: 2, delay: 2000 }),
+          catchError((err) => {
+            console.log('caught rethrown error', err);
+            return of('Http call failed');
+          }),
+          finalize(() => {
+            console.log('finally block 2');
+          })
+        )
+        .subscribe({
+          next: (data) => {
+            console.log(data);
+            this.data = data;
+          },
+          error: (err) => console.log('error occured in observable', err),
+        });
+      // const test = 'test';
+      // this.form[test]();
+    } catch (err) {
+      console.log('error occured', err);
+    }
+  }
+}
---
+++ src/app/shared.service.ts
@@ -0,0 +1,13 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class SharedService {
+  constructor(private http: HttpClient) {}
+
+  getData() {
+    return this.http.get('/test');
+  }
+}
