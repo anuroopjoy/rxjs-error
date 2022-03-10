2e48548f62dd7edd79dbdf2776b38ad4bcd32a31
Anuroop Joy <anuroop.joy@hrblock.com>
2022-03-10 15:55:05 +0530

    async

--- src/app/child1/child1.component.ts
+++ src/app/child1/child1.component.ts
@@ -19,13 +19,14 @@ import { SharedService } from '../shared.service';
   styleUrls: ['./child1.component.scss'],
 })
 export class Child1Component implements OnInit {
-  public data!: any;
-  private form!: any;
+  // public data!: any;
+  public data$!: Observable<any>;
+  // private form!: any;
   constructor(private sharedService: SharedService) {}
 
   ngOnInit(): void {
     try {
-      this.sharedService
+      this.data$ = this.sharedService
         .getData()
         // .pipe(catchError(() => of('Http call failed')))
         .pipe(
@@ -44,14 +45,14 @@ export class Child1Component implements OnInit {
           finalize(() => {
             console.log('finally block 2');
           })
-        )
-        .subscribe({
-          next: (data) => {
-            console.log(data);
-            this.data = data;
-          },
-          error: (err) => console.log('error occured in observable', err),
-        });
+        );
+      // .subscribe({
+      //   next: (data) => {
+      //     console.log(data);
+      //     this.data = data;
+      //   },
+      //   error: (err) => console.log('error occured in observable', err),
+      // });
       // const test = 'test';
       // this.form[test]();
     } catch (err) {
